77cef2df196571463930ea22057777ef36455154
xiongjian <webjsforyou@163.com>
2019-03-09 09:24:31 +0800

    '第一个nodejs文件'

---
+++ 01.nodejs/day01/01.helloNode.js
@@ -0,0 +1,17 @@
+/*
+  nodejs运行方法：
+    1. node xxx.js 来当当前文件目录
+    2. 在webstorm中 右键--run
+ */
+console.log('hello node111');
+
+/*
+  使用git管理仓库
+    1. 管理本地仓库
+      - git init -y
+      - 建立git忽略文件  .gitignore  只能忽略工作区文件
+      - git add . / * / -A
+      - git commit -m 'xxxxx'
+    2. 创建远程仓库
+      github创建
+ */
